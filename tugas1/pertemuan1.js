c9866a70d2c19c428bcd61b62f9ed094c577db8c
saskiakarmilaa <saskiakarmilaa@gmail.com>
2023-09-08 11:03:31 +0700

    push pertama

---
+++ tugas1/pertemuan1.js
@@ -0,0 +1,28 @@
+var hasil = 12;
+var hasil = 13;
+
+
+//deklarasi
+let tes = 12;
+//ubah value
+ tes = 13;
+
+
+ const book = 12;
+ //gakbsa diubah nilainya
+    // book = 13;
+console.log(book);
+
+//es6
+const counter2 = (bil1=3) => {
+    console.log(`test arrow fn ${bil1}`);
+}
+
+counter2(6)
+//es5 > kebawah
+// counter1()
+function counter1()  {
+
+    console.log("Test call");
+
+}
